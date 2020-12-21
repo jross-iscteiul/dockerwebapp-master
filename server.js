5e103391863e1c6f8a4b183fc39090febdbf4540
jross-iscteiul <jross@iscte-iul.pt>
2020-12-21 18:43:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT, HOST);
